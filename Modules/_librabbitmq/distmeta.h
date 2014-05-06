353e75cb5305dd82b58f5ef00844e689bb09fc85
Ask Solem <ask@celeryproject.org>
2014-05-06 13:52:02 +0100

    Bumps version to 1.5.1 and updates Changelog

--- Modules/_librabbitmq/distmeta.h
+++ Modules/_librabbitmq/distmeta.h
@@ -1,4 +1,4 @@
-#define PYRABBITMQ_VERSION "1.5.0"
+#define PYRABBITMQ_VERSION "1.5.1"
 #define PYRABBITMQ_AUTHOR "Ask Solem"
 #define PYRABBITMQ_CONTACT "ask@celeryproject.org"
 #define PYRABBITMQ_HOMEPAGE "http://github.com/celery/librabbitmq"
